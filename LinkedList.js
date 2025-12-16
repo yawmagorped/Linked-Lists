042c77416c65cc720e41850b2b1d40b1c27a01cb
ali <193356044+yawmagorped@users.noreply.github.com>
2025-12-16 18:26:47 +0330

    fixed appened and prepend and added printNode function (oh and also added size function to LinkedList)

--- LinkedList.js
+++ LinkedList.js
@@ -1,32 +1,51 @@
 const LinkedList = () => {
     let head = Node();
     let tail = Node();
+    head.setValue("head");
+    tail.setValue("tail");
 
     const append = (value) => {
         let newNode = Node();
         if (head.getNextNode() === null) { //first element
-            newNode.initialize(value, null, null);
+            newNode.initialize(value, head, tail);
             head.setNextNode(newNode);
             tail.setPrevNode(newNode);
+
         } else {
             newNode.initialize(value, tail.getPrevNode(), null);
+            tail.getPrevNode().setNextNode(newNode);
             tail.setPrevNode(newNode);
+            newNode.setNextNode(tail);
         }
     } 
-
+    
     const prepend = (value) => {
         let newNode = Node();
         if (head.getNextNode() === null) {
-            newNode.initialize(value, null, null);
+            newNode.initialize(value, head, tail);
             head.setNextNode(newNode);
             tail.setPrevNode(newNode);
         } else {
             newNode.initialize(value, null, head.getNextNode());
+            head.getNextNode().setPrevNode(newNode);
             head.setNextNode(newNode);
+            newNode.setPrevNode(head);
         }
     }
+    
+    const size = () => {
+        let counter = 0;
+        let node = head.getNextNode();
+        // console.log(head.getNextNode().getValue());
+        // console.log(head.getNextNode().getNextNode().getValue());
+        while(node !== null && node.getNextNode() !== null) {
+            node = node.getNextNode();
+            counter++;
+        }
+        return counter;
+    }
 
-    return {append, prepend};
+    return {append, prepend, size};
 }
 
 const Node = () => {
@@ -40,6 +59,13 @@ const Node = () => {
         _nextNode = next;
     }
 
+    const printNode = (name) => {
+        console.log("this is -> " + name);
+        console.log("value:" + _value);
+        console.log("nextNode:" + _nextNode().getValue());
+        console.log("prevNode:" + _prevNode().getValue());
+    }
+
     const copyNode = (node) => {
         _value = node.value;
         _nextNode = node.nextNode;
@@ -47,7 +73,7 @@ const Node = () => {
     }
 
     const setValue = (value) => {
-        value = value;
+        _value = value;
     } 
     const getValue = () => {
         return _value;
@@ -65,11 +91,17 @@ const Node = () => {
         return _prevNode;
     } 
 
-    return {initialize, setNextNode, getNextNode, setPrevNode, getPrevNode, setValue, getValue, copyNode};
+    return {initialize, setNextNode, getNextNode, setPrevNode, getPrevNode, setValue, getValue, copyNode, printNode};
 }
 
 let linkedList = LinkedList();
 
+console.log(linkedList.size());
 linkedList.append(1);
-linkedList.append(2);
+console.log(linkedList.size());
+linkedList.prepend(2);
+console.log(linkedList.size());
 linkedList.append(3);
+console.log(linkedList.size());
+linkedList.prepend(4);
+console.log(linkedList.size());
