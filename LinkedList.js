10a34f9fdf79087c51b05133a2a75097dee41cf8
ali <193356044+yawmagorped@users.noreply.github.com>
2025-12-16 17:31:34 +0330

    Implemented appened and prepend functions to LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -1,40 +1,75 @@
 const LinkedList = () => {
-    let head = null;
-    let tail = null;
+    let head = Node();
+    let tail = Node();
 
     const append = (value) => {
         let newNode = Node();
-        if (head === null) { //first element
+        if (head.getNextNode() === null) { //first element
             newNode.initialize(value, null, null);
-            head = newNode;
-            tail = newNode;
+            head.setNextNode(newNode);
+            tail.setPrevNode(newNode);
         } else {
-            newNode.initialize(value, tail, null);
-            tail.nextNode = newNode;
-            tail = newNode;
+            newNode.initialize(value, tail.getPrevNode(), null);
+            tail.setPrevNode(newNode);
         }
     } 
 
-    return {};
+    const prepend = (value) => {
+        let newNode = Node();
+        if (head.getNextNode() === null) {
+            newNode.initialize(value, null, null);
+            head.setNextNode(newNode);
+            tail.setPrevNode(newNode);
+        } else {
+            newNode.initialize(value, null, head.getNextNode());
+            head.setNextNode(newNode);
+        }
+    }
+
+    return {append, prepend};
 }
 
 const Node = () => {
-    let value;
-    let nextNode = null;
-    let prevNode = null;
+    let _value;
+    let _nextNode = null;
+    let _prevNode = null;
 
     const initialize = (value, prev, next) => {
-        value = value;
-        prev = prev;
-        next = next;
+        _value = value;
+        _prevNode = prev;
+        _nextNode = next;
     }
 
-    const setNext = (node) => {
-        nextNode = node.nextNode;
+    const copyNode = (node) => {
+        _value = node.value;
+        _nextNode = node.nextNode;
+        _prevNode = node.prevNode;
+    }
+
+    const setValue = (value) => {
+        value = value;
     } 
-    const setPrev = (node) => {
-        prevNode = node.prevNode;
+    const getValue = () => {
+        return _value;
+    }
+    const setNextNode = (node) => {
+        _nextNode = node;
+    } 
+    const getNextNode = () => {
+        return _nextNode;
+    }
+    const setPrevNode = (node) => {
+        _prevNode = node;
+    }
+    const getPrevNode = () => {
+        return _prevNode;
     } 
 
-    return {initialize, setNext, setPrev};
+    return {initialize, setNextNode, getNextNode, setPrevNode, getPrevNode, setValue, getValue, copyNode};
 }
+
+let linkedList = LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
